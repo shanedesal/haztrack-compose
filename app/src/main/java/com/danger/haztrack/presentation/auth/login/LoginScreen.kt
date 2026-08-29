781b00b0e4584525c4dad926c99f4d2f88c4539b
shanedesal <shanegamboa566@gmail.com>
2026-08-29 15:04:32 +0800

    style: added icons, tentative

--- app/src/main/java/com/danger/haztrack/presentation/auth/login/LoginScreen.kt
+++ app/src/main/java/com/danger/haztrack/presentation/auth/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.danger.haztrack.presentation.auth.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -30,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -111,11 +113,10 @@ private fun LoginContent(
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Spacer(modifier = Modifier.height(56.dp))
-            Icon(
-                imageVector = Icons.Filled.LocalShipping,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.height(48.dp),
+            Image(
+                painter = painterResource(R.drawable.ic_app_logo),
+                contentDescription = stringResource(R.string.app_name),
+                modifier = Modifier.height(72.dp)
             )
             Spacer(modifier = Modifier.height(16.dp))
             Text(
